fdf3db77bf4cf2af35433d6b6f5990cb1ed64a36
Amir <Amir@AAO-Desktop>
2013-11-08 01:57:51 +0330

    First Commit

---
+++ src/main/java/dev/mccn/co/mccnskyblock/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dev.mccn.co.mccnskyblock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Amir
+ */
+public class Main extends JavaPlugin{
+    
+}
